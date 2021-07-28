986c5cb797c88a55094f92a12146738a892e3c67
monjaburkard <monja.burkard@gmail.com>
2021-07-28 21:44:11 +0200

    update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='probability_distributions-egv',
-        version='0.1',
-        description='probability distributions-egv',
-      packages=['probability_distributions-egv'],
+setup(name='probability_distributions_egv',
+        version='1.1',
+        description='probability distributions_egv',
+      packages=['probability_distributions_egv'],
       zip_safe=False)
